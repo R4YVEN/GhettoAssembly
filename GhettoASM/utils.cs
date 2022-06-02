01cf3a2b8d48152a93ad14565dbc56cee28b6715
Emlin <49819627+R4YVEN@users.noreply.github.com>
2022-06-02 18:57:56 +0200

    remove useless

--- GhettoASM/utils.cs
+++ GhettoASM/utils.cs
@@ -152,29 +152,5 @@ namespace GhettoASM
                 return obj;
             }
         }
-
-        internal static StructureType ReadStructure<StructureType>(Stream Stream)
-    where StructureType : struct
-        {
-            int Length = Marshal.SizeOf(typeof(StructureType));
-            byte[] Bytes = new byte[Length];
-            Stream.Read(Bytes, 0, Length);
-            IntPtr Handle = Marshal.AllocHGlobal(Length);
-            Marshal.Copy(Bytes, 0, Handle, Length);
-            StructureType Result = (StructureType)Marshal.PtrToStructure(Handle, typeof(StructureType));
-            Marshal.FreeHGlobal(Handle);
-            return Result;
-        }
-
-        internal static void WriteStructure(object Structure, Stream Stream)
-        {
-            int Length = Marshal.SizeOf(Structure);
-            byte[] Bytes = new byte[Length];
-            IntPtr Handle = Marshal.AllocHGlobal(Length);
-            Marshal.StructureToPtr(Structure, Handle, true);
-            Marshal.Copy(Handle, Bytes, 0, Length);
-            Marshal.FreeHGlobal(Handle);
-            Stream.Write(Bytes, 0, Length);
-        }
     }
 }
